93866b25f443ba3850aa9b6800a0428bd614f71c
Ivan Galarza <id.galarza.sa@gmail.com>
2020-06-03 01:41:05 -0400

    landscape mode for movie info

--- app/src/main/java/com/galarzaIvan/movies/MovieInfoActivity.java
+++ app/src/main/java/com/galarzaIvan/movies/MovieInfoActivity.java
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
